70085c9144ad50856aee0bda5240d29f1bb0ee5d
svd <serednev_valentin@gmail.com>
2021-03-25 00:59:02 +0300

    25032021_01:00

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import java.util.stream.Stream;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -24,12 +22,45 @@ public class Main {
         int s4Int = Integer.parseInt(s4);
         s4Int = s4Int - 1;
 
+        //String s5Final_1 = s5.substring(0,5)+'и'+s5.substring(5);
+
+        StringBuffer s5 = new StringBuffer("сосед и децибелы");
+        s5.delete(5,6);
+        s5.setCharAt(7,'и');
+        s5.setCharAt(8,'м');
+
+        String s5_2 = s5.toString();
+
+        char[] c = s5_2.toCharArray();
+        char temp = c[9];
+        c[9] = c[11];
+        c[11] = temp;
+
+        String s5_3 = new String(c);
+
+        StringBuffer s5_4 = new StringBuffer(s5_3);
+        s5_4.setCharAt(10,'e');
+        s5_4.setCharAt(12,'и');
+
+
+//        String s5_4 = c.toString();
+
+//        char[] d = s5_4.toCharArray();
+//        char temp = c[12];
+//        d[12] = d[10];
+//        d[10] = temp;
+        // вероятно, нет возможности повторно испольщзовать зарезервированное слово "temp"
+
+
 
 
         System.out.println("1.  " + "before: " + s1 + "\n" + "    after: " + s1Final);
         System.out.println("2.  " + "before: " + s2 + "\n" + "    after: " + s2Final);
         System.out.println("3.  " + "before: " + s3 + "\n" + "    after: " + s3Final);
         System.out.println("4.  " + "before: " + s3Final + "\n" + "    after: " + s4Int);
-
+        System.out.println(s5);
+        System.out.println(s5_2);
+        System.out.println(s5_3);
+        System.out.println(s5_4);
 
 }}
